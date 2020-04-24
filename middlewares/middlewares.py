d61c79572e8cf8dbdb2b1d9a1a07111b73717cb9
WardPearce <wpearce6@gmail.com>
2020-04-25 06:02:00 +1200

    Update middlewares.py

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -4,9 +4,9 @@ from .api_key import APIKeyValidation
 
 class Middlewares(object):
     def __init__(self, obj):
-        self.api_key_validation = APIKeyValidation
-        self.api_key_validation.obj = obj
+        api_key_validation = APIKeyValidation
+        api_key_validation.obj = obj
 
         self.list = [
-            Middleware(self.api_key_validation)
+            Middleware(api_key_validation)
         ]
