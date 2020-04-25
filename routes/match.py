3fcde47d10b6cdbed011fde8733b982e609653fb
WardPearce <wpearce6@gmail.com>
2020-04-25 22:55:57 +1200

    Match post fields are required

--- routes/match.py
+++ routes/match.py
@@ -4,8 +4,8 @@ from webargs import fields
 from webargs_starlette import use_args
 
 class Match(HTTPEndpoint):
-    @use_args({"players": fields.Dict(), "maps": fields.Dict(),
-               "team_names": fields.Dict(),})
+    @use_args({"players": fields.Dict(required=True), "maps": fields.Dict(required=True),
+               "team_names": fields.Dict(required=True),})
     async def post(self, request, args):
         """ Creates match. """
 
