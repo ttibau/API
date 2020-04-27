1e3308254bf9acbfa2183d153beee38c48b25e89
WardPearce <wpearce6@gmail.com>
2020-04-27 13:10:06 +1200

    Started scoreboard model

---
+++ models/scoreboard.py
@@ -0,0 +1,11 @@
+class ScoreboardModel:
+    def __init__(self, data: dict):
+        self.data = data
+
+    @property
+    def full(self):
+        """ Formats and returns full response for scoreboard. """
+
+        pass
+
+    
