60a5d592c3eb56acaff7efc3a17da8d1146a3fb8
WardPearce <wpearce6@gmail.com>
2020-04-27 12:41:23 +1200

    Update match.py

--- modules/match.py
+++ modules/match.py
@@ -226,7 +226,25 @@ class Match(object):
 
         row = await self.current_league.obj.database.fetch_one(query=query, values=values)
         if row:
-            return response(data={**row})
+            return response(data={
+                "match_id": row["match_id"],
+                "server_id": row["server_id"],
+                "map": row["map"],
+                "status": row["status"],
+                "timestamp": row["timestamp"],
+                "map_order": row["map_order"],
+                "player_order": row["player_order"],
+                "team_1": {
+                    "name": row["team_1_name"],
+                    "score": row["team_1_score"],
+                    "side": row["team_1_side"],
+                },
+                "team_2": {
+                    "name": row["team_2_name"],
+                    "score": row["team_2_score"],
+                    "side": row["team_2_side"],
+                },
+            })
         else:
             return response(error="No match with that ID")
 
