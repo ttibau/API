f94e50c9de371b0c9e3c1909f6c67a2ae7796fed
WardPearce <wardpearce@protonmail.com>
2020-05-11 15:40:20 +1200

    Update errors.py

--- routes/errors.py
+++ routes/errors.py
@@ -3,11 +3,13 @@ from utils.responder import responder
 
 
 class Errors:
+    @staticmethod
     async def http_exception(request, exc):
         return responder.render(
             response(error=exc.detail, status=exc.status_code)
         )
 
+    @staticmethod
     async def arg_expection(request, exc):
         return responder.render(
             response(error=exc.messages, status=exc.status_code)
